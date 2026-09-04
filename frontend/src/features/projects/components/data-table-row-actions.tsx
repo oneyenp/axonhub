650c35b1f6ed2122104acb9108c1f2e45e993c96
oneyenp <oneyenp@gmail.com>
2026-09-04 16:33:01 +0800

    feat: add storage policy action to projects

--- frontend/src/features/projects/components/data-table-row-actions.tsx
+++ frontend/src/features/projects/components/data-table-row-actions.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { Row } from '@tanstack/react-table';
-import { IconEdit, IconSettings, IconTrash } from '@tabler/icons-react';
+import { IconDatabaseCog, IconEdit, IconSettings, IconTrash } from '@tabler/icons-react';
 import { useTranslation } from 'react-i18next';
 import { usePermissions } from '@/hooks/usePermissions';
 import { Button } from '@/components/ui/button';
@@ -14,6 +14,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { useProjectsContext } from '../context/projects-context';
 import { Project } from '../data/schema';
+import { ProjectStoragePolicyDialog } from './project-storage-policy-dialog';
 
 interface DataTableRowActionsProps {
   row: Row<Project>;
@@ -25,6 +26,7 @@ export function DataTableRowActions({ row }: DataTableRowActionsProps) {
   const { setEditingProject, setArchivingProject, setActivatingProject, setDeletingProject, setProfilesProject } = useProjectsContext();
   const { projectPermissions } = usePermissions();
   const [open, setOpen] = React.useState(false);
+  const [storagePolicyOpen, setStoragePolicyOpen] = React.useState(false);
 
   // Don't show menu if user has no permissions
   if (!projectPermissions.canWrite && !projectPermissions.canDelete) {
@@ -34,6 +36,11 @@ export function DataTableRowActions({ row }: DataTableRowActionsProps) {
   const handleProfiles = () => {
     setOpen(false);
     setTimeout(() => setProfilesProject(project), 0);
+  };
+
+  const handleStoragePolicy = () => {
+    setOpen(false);
+    setTimeout(() => setStoragePolicyOpen(true), 0);
   };
 
   const handleEdit = () => {
@@ -57,58 +64,70 @@ export function DataTableRowActions({ row }: DataTableRowActionsProps) {
   };
 
   return (
-    <DropdownMenu open={open} onOpenChange={setOpen}>
-      <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className='data-[state=open]:bg-muted flex h-8 w-8 p-0'>
-          <DotsHorizontalIcon className='h-4 w-4' />
-          <span className='sr-only'>{t('common.actions.openMenu')}</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end' className='w-[160px]'>
-        {/* Profiles - requires write permission */}
+    <>
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='data-[state=open]:bg-muted flex h-8 w-8 p-0'>
+            <DotsHorizontalIcon className='h-4 w-4' />
+            <span className='sr-only'>{t('common.actions.openMenu')}</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end' className='w-[190px]'>
+          {/* Edit - requires write permission */}
+          {projectPermissions.canEdit && (
+            <DropdownMenuItem onClick={handleEdit}>
+              <IconEdit className='mr-2 h-4 w-4' />
+              {t('common.actions.edit')}
+            </DropdownMenuItem>
+          )}
 
+          {projectPermissions.canWrite && (
+            <>
+              <DropdownMenuItem onClick={handleProfiles}>
+                <IconSettings className='mr-2 h-4 w-4' />
+                {t('projects.profiles.title')}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={handleStoragePolicy}>
+                <IconDatabaseCog className='mr-2 h-4 w-4' />
+                {t('projects.storagePolicy.title')}
+              </DropdownMenuItem>
+            </>
+          )}
 
-        {/* Edit - requires write permission */}
-        {projectPermissions.canEdit && (
-          <DropdownMenuItem onClick={handleEdit}>
-            <IconEdit className='mr-2 h-4 w-4' />
-            {t('common.actions.edit')}
-          </DropdownMenuItem>
-        )}
+          {projectPermissions.canEdit && projectPermissions.canWrite && <DropdownMenuSeparator />}
 
-        {projectPermissions.canWrite && (
-          <DropdownMenuItem onClick={handleProfiles}>
-            <IconSettings className='mr-2 h-4 w-4' />
-            {t('projects.profiles.title')}
-          </DropdownMenuItem>
-        )}
+          {/* Archive - requires write permission, only for active projects */}
+          {projectPermissions.canWrite && project.status === 'active' && (
+            <DropdownMenuItem onClick={handleArchive} className='text-destructive focus:text-destructive'>
+              <IconTrash className='mr-2 h-4 w-4' />
+              {t('common.buttons.archive')}
+            </DropdownMenuItem>
+          )}
 
-        {projectPermissions.canEdit && projectPermissions.canWrite && <DropdownMenuSeparator />}
+          {/* Activate - requires write permission, only for archived projects */}
+          {projectPermissions.canWrite && project.status === 'archived' && (
+            <DropdownMenuItem onClick={handleActivate}>
+              <IconEdit className='mr-2 h-4 w-4' />
+              {t('common.buttons.activate')}
+            </DropdownMenuItem>
+          )}
 
-        {/* Archive - requires write permission, only for active projects */}
-        {projectPermissions.canWrite && project.status === 'active' && (
-          <DropdownMenuItem onClick={handleArchive} className='text-destructive focus:text-destructive'>
-            <IconTrash className='mr-2 h-4 w-4' />
-            {t('common.buttons.archive')}
-          </DropdownMenuItem>
-        )}
+          {/* Delete - requires owner permission */}
+          {projectPermissions.canDelete && (
+            <DropdownMenuItem onClick={handleDelete} className='text-destructive focus:text-destructive'>
+              <IconTrash className='mr-2 h-4 w-4' />
+              {t('common.actions.delete')}
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-        {/* Activate - requires write permission, only for archived projects */}
-        {projectPermissions.canWrite && project.status === 'archived' && (
-          <DropdownMenuItem onClick={handleActivate}>
-            <IconEdit className='mr-2 h-4 w-4' />
-            {t('common.buttons.activate')}
-          </DropdownMenuItem>
-        )}
-
-        {/* Delete - requires owner permission */}
-        {projectPermissions.canDelete && (
-          <DropdownMenuItem onClick={handleDelete} className='text-destructive focus:text-destructive'>
-            <IconTrash className='mr-2 h-4 w-4' />
-            {t('common.actions.delete')}
-          </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <ProjectStoragePolicyDialog
+        projectId={project.id}
+        projectName={project.name}
+        open={storagePolicyOpen}
+        onOpenChange={setStoragePolicyOpen}
+      />
+    </>
   );
 }
